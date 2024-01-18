f2ec29b91145d83c0007f874628c58986c2dc689
Maria Lourdes Garcia <mut_mgarcia@externos.entel.cl>
2024-01-18 01:15:09 -0300

    Styled component main container

---
+++ client/src/shared/main-container.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+export const BaseContent = styled.div`
+  max-width: 1140px;
+  width: 100%;
+  padding: 100px;
+`;
