0a97c95089305dea6d4a2ff2607e1c6419de37c2
CristCT <crist.fract@gmail.com>
2024-01-22 15:46:17 -0300

    add functions to search and delete

--- desafio-react-agenda-frontend/src/components/UserList.tsx
+++ desafio-react-agenda-frontend/src/components/UserList.tsx
@@ -3,10 +3,11 @@ import { Table, Input, Button, Space, Pagination } from 'antd';
 import { ColumnType } from 'antd/es/table';
 import { PlusOutlined, DeleteOutlined, UserOutlined } from '@ant-design/icons';
 import { IUser } from '../types/userTypes';
-import { getUsers } from '../services/apiService';
+import { getUsers, deleteUser } from '../services/apiService';
 
 const UserList: React.FC = () => {
   const [users, setUsers] = useState<IUser[]>([]);
+  const [allUsers, setAllUsers] = useState<IUser[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
   const [searchText, setSearchText] = useState<string>('');
@@ -17,11 +18,12 @@ const UserList: React.FC = () => {
   useEffect(() => {
     setLoading(true);
     getUsers(currentPage, pageSize)
-      .then(response => {
-        setUsers(response.data);
+      .then((response) => {
+        setUsers(response.data); // Almacena los usuarios de la página actual
+        setAllUsers(response.data); // Almacena todos los usuarios
         setTotalUsers(response.total);
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error.message);
       })
       .finally(() => {
@@ -35,7 +37,7 @@ const UserList: React.FC = () => {
       key: 'name',
       render: (_, record: IUser) => (
         <Space>
-          {(record.photo && record.photo.endsWith('404.png')) ? (
+          {record.photo && record.photo.endsWith('404.png') ? (
             <UserOutlined style={{ fontSize: '30px' }} />
           ) : (
             <img
@@ -45,11 +47,16 @@ const UserList: React.FC = () => {
               onError={(e) => {
                 e.currentTarget.style.display = 'none'; // Oculta la imagen en caso de error
 
-                // Verificar si el error es 404 (Not Found)
+                // Verifica si el error es 404 (Not Found)
                 if (e.currentTarget.naturalWidth === 0) {
-                  // Mostrar el ícono UserOutlined como marcador de posición
+                  // Muestra el ícono UserOutlined como marcador de posición
                   if (e.currentTarget.parentNode) {
-                    (e.currentTarget.parentNode as HTMLElement).insertAdjacentHTML('beforeend', '<span><UserOutlined style={{ fontSize: "30px" }} /></span>');
+                    (
+                      e.currentTarget.parentNode as HTMLElement
+                    ).insertAdjacentHTML(
+                      'beforeend',
+                      '<span><UserOutlined style={{ fontSize: "30px" }} /></span>',
+                    );
                   }
                 }
               }}
@@ -73,39 +80,68 @@ const UserList: React.FC = () => {
     },
   ];
 
-  // Funciones handleDelete y handleSearch
-  const handleDelete = (userId: number) => {
-    // Lógica para eliminar un usuario
-    
+  // Funciones handleDelete, handleSearch y handlePageChange
+  const handleDelete = async (userId: number) => {
+    try {
+      await deleteUser(userId);
+      // Actualiza la lista de usuarios después de eliminar (realtime)
+      const updatedUsers = users.filter((user) => user.id !== userId);
+      setUsers(updatedUsers);
+    } catch (error) {
+      // Maneja cualquier error que pueda ocurrir durante la eliminación
+      console.error('Error al eliminar el usuario:', error);
+    }
   };
 
+  // Dentro de tu componente UserList
   const handleSearch = (value: string) => {
-    // Lógica para filtrar los usuarios
+    setSearchText(value.toLowerCase());
+    setLoading(true);
+    getUsers(1, pageSize, value) // Restablece a la primera página y pasa el término de búsqueda
+      .then(response => {
+        setUsers(response.data); // Actualiza con los usuarios filtrados
+        setTotalUsers(response.total); // Actualiza el total de usuarios (si tu API lo soporta)
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
+
 
   const handlePageChange = (page: number, pageSize: number) => {
     setCurrentPage(page);
     setPageSize(pageSize);
   };
 
-  // Filtrado de usuarios basado en el texto de búsqueda
-  const filteredUsers = searchText
-    ? users.filter((user) =>
-        user.name.toLowerCase().includes(searchText)
-      )
-    : users;
-
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div>
       <h1>Agenda Previred - Mi agenda de contactos laboral</h1>
-      <p>Aquí podrá encontrar o buscar a todos sus contactos agregados, agregar nuevos contactos y eliminar contactos no deseados.</p>
-      <div style={{ marginBottom: 16, display: 'flex', flexDirection: 'column', padding: '0 20px', alignItems: 'flex-start' }}>
+      <p>
+        Aquí podrá encontrar o buscar a todos sus contactos agregados, agregar
+        nuevos contactos y eliminar contactos no deseados.
+      </p>
+      <div
+        style={{
+          marginBottom: 16,
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '0 20px',
+          alignItems: 'flex-start',
+        }}
+      >
         <div style={{ width: '100%' }}>
           <div style={{ marginBottom: 16, width: '20%' }}>
-            <Button type="primary" icon={<PlusOutlined />} style={{ width: '100%' }}>
+            <Button
+              type="primary"
+              icon={<PlusOutlined />}
+              style={{ width: '100%' }}
+            >
               Agregar Contacto
             </Button>
           </div>
@@ -118,7 +154,12 @@ const UserList: React.FC = () => {
           </div>
         </div>
       </div>
-      <Table columns={columns} dataSource={filteredUsers} rowKey="id" pagination={false} />
+      <Table
+        columns={columns}
+        dataSource={users}
+        rowKey="id"
+        pagination={false}
+      />
 
       <Pagination
         current={currentPage}
--- desafio-react-agenda-frontend/src/services/apiService.ts
+++ desafio-react-agenda-frontend/src/services/apiService.ts
@@ -3,17 +3,20 @@ import { IUser } from '../types/userTypes';
 
 const API_BASE_URL = 'http://localhost:9000/api';
 
-export const getUsers = async (page: number, limit: number): Promise<{data: IUser[], total: number}> => {
+// src/services/apiService.ts
+
+export const getUsers = async (page: number, limit: number, searchQuery?: string): Promise<{data: IUser[], total: number}> => {
   const response = await axios.get(`${API_BASE_URL}/users`, {
-      params: {
-          _page: page,
-          _limit: limit
-      }
+    params: {
+      _page: page,
+      _limit: limit,
+      q: searchQuery
+    }
   });
   const total = parseInt(response.headers['x-total-count'], 10);
   return {
-      data: response.data,
-      total: total
+    data: response.data,
+    total: total
   };
 };
 
@@ -23,11 +26,11 @@ export const getUser = async (id: number): Promise<IUser> => {
 };
 
 export const createUser = async (user: IUser): Promise<IUser> => {
-    const response = await axios.post(`${API_BASE_URL}/users`, user);
-    return response.data;
+  const response = await axios.post(`${API_BASE_URL}/users`, user);
+  return response.data;
 };
 
 export const deleteUser = async (id: number): Promise<IUser> => {
-    const response = await axios.delete(`${API_BASE_URL}/users/${id}`);
-    return response.data;
+  const response = await axios.delete(`${API_BASE_URL}/users/${id}`);
+  return response.data;
 };
