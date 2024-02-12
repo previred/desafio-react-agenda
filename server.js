ce475ddb440ebb826c972b6224aec70116739f73
Alejandro Silva <pm5k.sk@gmail.com>
2024-02-12 18:37:57 -0300

    Feat/setup react and typescript (#1)
    * chore: add new folders to .gitignore
    
    * chore: add vite dependencies and a default config
    
    * feat: render a basic React component
    
    * feat: serve the /dist folder only on "production"
    
    * chore: update the lockfileVersion to version 2

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const jsonServer = require("json-server");
+const express = require('express');
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const PORT = 9000;
 
+if(process.env.NODE_ENV === 'production') {
+  console.log('Serving static files from the "/dist" folder')
+  server.use(express.static('dist'));
+}
+
 server.use(middlewares);
 server.use("/api", router);
 server.listen(9000, () => {
-  console.log(`JSON Server is running in http://locahost:${PORT}`);
+  console.log(`JSON Server is running in http://localhost:${PORT}`);
 });
