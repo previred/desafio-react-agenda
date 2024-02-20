c345b10900daaad9f7e110570d25ce2ed55c7dcc
Eve <eveninosdu@gmail.com>
2024-02-20 00:39:15 -0300

    chore: refactor search input

---
+++ src/containers/SearchInput/SearchInput.tsx
@@ -0,0 +1,7 @@
+import { Input } from "../../components/Input";
+
+import "./SearchInput.scss";
+
+export const SearchInput = () => {
+  return <Input placeholder="Buscar contactos..." className="search-input" />;
+};
